4fc179996d500f388fe2f12afc7cc23ba0deb177
Livya Kendi Kiruja <livyakendikiruja@gmail.com>
2023-01-27 23:13:46 +0300

    Loops, oh loops

--- index.js
+++ index.js
@@ -1,13 +1,32 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+  for (let i = 0; i < array.length; i++) {
+
+    //find a number in the array that currently complements our target when summed up
+    //this number is called sumArray. When added to a number in the array it should 
+    // return the target.
+    const sumArray = target - array[i];
+
+    //loop through all the numbers in the array
+    for (let j = i + 1; j < array.length; j++){
+      if (array[j] === sumArray) 
+      return true;
+    }
+  }
+  return false;
 }
 
 /* 
   Write the Big O time complexity of your function here
+  Quadratic Time
 */
 
 /* 
   Add your pseudocode here
+  loop through the array and find each number
+  pair two numbers and sum them up
+  if any two numbers in the array sum up to target return true
+  if not return false
 */
 
 /*
